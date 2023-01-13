793e4b6cc562a9e0139b9d93a1fb39d6c5612d21
zkitefly <64117916+zkitefly@users.noreply.github.com>
2023-01-13 23:24:48 +0800

    Update I18nModConfig.java

--- src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
+++ src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
@@ -11,10 +11,10 @@ public class I18nModConfig {
         @Config.Name("资源包下载源")
         @Config.Comment(
                 "CFPA官方下载源：http://downloader1.meitangdehulu.com:22943  \n" +
-                "镜像下载源：https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackConvert/main/files  \n" +
+                "镜像下载源：https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackMirror/main/files  \n" +
                 "注意：链接最后不要带任何符号（如：/）"
         )
-        public String downloadLink = "https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackConvert/main/files";
+        public String downloadLink = "https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackMirror/main/files";
 
         @Config.Name("资源包名称")
         @Config.Comment("用来设置获取下载源的资源包名称（注意：不带文件扩展名！）")
